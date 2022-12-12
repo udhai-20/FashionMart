a6de3b771bacc36a38cc8284fd4ad1261ba29419
udhai-20 <udhayaroger511@gmail.com>
2022-12-12 19:21:16 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./Redux/store";
-import { Provider } from "react-redux";
+// import { store } from "./Redux/store";
+// import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
