8cd75725ffae15eb25a713704f66d1c4e8652cdd
udhai-20 <udhayaroger511@gmail.com>
2022-12-13 22:56:23 +0530

    day-1

--- FrontEnd/src/Pages/SingleProduct/SingleProduct.jsx
+++ FrontEnd/src/Pages/SingleProduct/SingleProduct.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import { Container } from "@chakra-ui/react";
 function SingleProduct(props) {
-  return <div>SingleProduct</div>;
+  return <Container></Container>;
 }
 
 export default SingleProduct;
