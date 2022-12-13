3db887bbb0ff8bfaba13164e1bce75dac4ede302
udhai-20 <udhayaroger511@gmail.com>
2022-12-13 10:43:12 +0530

    redux updated

---
+++ src/Redux/AuthReducer/reducers.js
@@ -0,0 +1,11 @@
+
+const initialize ={
+    isLoading:false,
+    isError:false,
+    isAuth:false,
+    token:null,
+}
+
+export const reducers = (state = initialize, action)=>{
+    return state
+}
