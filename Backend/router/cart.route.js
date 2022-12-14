abf93031e24e2f56027197764e324affadfc2c33
pandit0305 <ptgangadhar.may3@gmail.com>
2022-12-14 18:31:20 +0530

    authentication done

---
+++ Backend/router/cart.route.js
@@ -0,0 +1,69 @@
+const express = require("express");
+
+const cartRouter = express.Router();
+
+const { cartModel } = require("../model/cart.model");
+
+cartRouter.get("/", async (req, res) => {
+  try {
+    const getdata = await cartModel.find();
+
+    res.send(getdata);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+cartRouter.post("/create", async (req, res) => {
+  try {
+    const payload = req.body;
+    console.log(payload)
+    const new_note = new cartModel(payload);
+    await new_note.save();
+    res.send({ message: "note is created" });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
+// cartRouter.put("/update/:cartId", async (req, res) => {
+//   try {
+//     const payload = req.body;
+//     const cartId = req.params.cartId;
+
+//     const cartID = req.body.cartId;
+//     console.log(cartID);
+//     const user = await cartModel.findOne({ _id: cartId });
+//     console.log(user);
+//     if (cartID !== user.cartId) {
+//       res.send("user is not authorized");
+//     } else {
+//       await cartModel.findByIdAndUpdate({ _id: cartId }, payload);
+//       res.send("note is updated");
+//     }
+//   } catch (err) {
+//     res.status(400).send({ message: err.message });
+//   }
+// });
+
+// cartRouter.delete("/delete/:cartId", async (req, res) => {
+//   try {
+  
+//     const cartId = req.params.cartId;
+
+//     const cartID = req.body.cartId;
+//     console.log(cartID);
+//     const user = await cartModel.findOne({ _id: cartId });
+//     console.log(user);
+//     if (cartID !== user.cartId) {
+//       res.send("user is not authorized");
+//     } else {
+//       await cartModel.findByIdAndDelete({ _id: cartId });
+//       res.send("note is deleted");
+//     }
+//   } catch (err) {
+//     res.status(400).send({ message: err.message });
+//   }
+// });
+
+module.exports = { cartRouter };
