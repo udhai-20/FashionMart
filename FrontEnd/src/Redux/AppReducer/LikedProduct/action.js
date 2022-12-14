81b33b7863088bd2dda705f53901d2f68a484241
udhai-20 <udhayaroger511@gmail.com>
2022-12-15 00:08:50 +0530

    redux structrue updated

---
+++ FrontEnd/src/Redux/AppReducer/LikedProduct/action.js
@@ -0,0 +1,78 @@
+import * as types from "./actionType";
+import axios from "axios";
+
+//GET DATA
+const get_failure = () =>({
+    type: types.GET_FAILURE_DATA
+})
+
+
+const get_success =(data)=>({
+    type:types.GET_SUCCESS_DATA,
+    payload: data
+})
+
+const get_request = ()=>({
+    type : types.GET_REQUEST_DATA
+})
+
+
+//POST ACTION
+const post_failure = () =>({
+    type: types.POST_FAILURE_DATA
+})
+
+// const post_success =()=>({
+//     type:types.POST_SUCCESS_DATA,
+// })
+
+const post_request = ()=>({
+    type : types.POST_REQUEST_DATA
+})
+
+//UPDATE ACTION
+
+const update_failure = () =>({
+    type: types.UPDATE_FAILURE_DATA
+})
+
+
+const update_success =()=>({
+    type:types.UPDATE_SUCCESS_DATA,
+})
+
+const update_request = ()=>({
+    type : types.UPDATE_REQUEST_DATA
+})
+
+//DELETE ACTION
+const delete_failure = () =>({
+    type: types.DELETE_FAILURE_DATA
+})
+
+
+const delete_success =()=>({
+    type:types.DELETE_SUCCESS_DATA,
+   
+})
+
+const delete_request = ()=>({
+    type : types.DELETE_REQUEST_DATA
+})
+
+
+
+// export const postdata =(dispatch)=>(payload, token)=>{
+//     dispatch(post_request)
+//     axios({
+//         method:"post",
+//         baseURL:"http:localhost:8080",
+//         url:"/notes/create",
+//         headers: {"Authorization":`${token}`},
+//         data:payload
+//     }).then((response) => {
+//         console.log(response);
+//     }).catch((err)=>{
+//         dispatch(post_failure(err))
+//     })
+// }
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-// import { store } from "./Redux/store";
-// import { Provider } from "react-redux";
+import { store } from "./Redux/store";
+import { Provider } from "react-redux";
+console.log("store:", store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <ChakraProvider>
-      {/* <Provider store={store}> */}
-      <App />
-      {/* </Provider> */}
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ChakraProvider>
   </BrowserRouter>
 );
